318ffcb3ec524517e4458c6f6e7dcde4ac6b1e95
Tom Kraak <thomas.kraak@teamaol.com>
2017-03-04 08:34:30 -0500

    configure spike-contentful

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config({ silent: true })
+
+const _ = require('lodash-addons')
 const htmlStandards = require('spike-html-standards')
 const cssStandards = require('spike-css-standards')
 const latest = require('babel-preset-latest')
+const Contentful = require('spike-contentful')
+const marked = require('marked')
+const locals = {}
 
 module.exports = {
   devtool: 'source-map',
@@ -10,12 +16,74 @@ module.exports = {
     return htmlStandards({
       parser: false,
       webpack: ctx,
-      locals: { foo: 'bar' }
+      locals: Object.assign(
+        locals,
+        { marked: marked },
+        { _: _})
     })
   },
   postcss: (ctx) => {
     return cssStandards({ parser: false, webpack: ctx })
   },
   babel: { presets: [latest] },
+  plugins: [
+    new Contentful({
+      addDataTo: locals,
+      accessToken: process.env.accessToken,
+      spaceId: process.env.spaceId,
+      contentTypes: [
+        {
+          name: 'index',
+          id: 'page',
+          filters: {
+            'sys.id': '4GP68pszosicYwowA2QuY2'
+          }
+        },
+        {
+          name: 'about',
+          id: 'page',
+          filters: {
+            'sys.id': '1ShmJTtfuo0mgCcIOAu6Qi'
+          }
+        },
+        {
+          name: 'contact',
+          id: 'page',
+          filters: {
+            'sys.id': '3ioJJTHOFyoOOksMuYEswa'
+          }
+        },
+        {
+          name: 'sales',
+          id: 'page',
+          filters: {
+            'sys.id': '60aMIbHglykAuUwgg2QkUO'
+          }
+        },
+        {
+          name: 'credit',
+          id: 'page',
+          filters: {
+            'sys.id': '31FKyKE2Huu6EAWI8YyIYK'
+          }
+        },
+        {
+          name: 'fourzerofour',
+          id: 'page',
+          filters: {
+            'sys.id': '7dP1qSSE6s6OQcIy6WmyEc'
+          }
+        },
+        {
+          name: 'vendors',
+          id: 'vendor',
+          filters: {
+            order: 'fields.title'
+          }
+        }
+      ],
+      json: 'data/data.json'
+    })
+  ],
   server: { open: false}
 }
